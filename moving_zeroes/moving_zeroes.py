2122816f49f5ee2c2aa7e148bfa8dcd0f134bf2e
roselandroche <r.landroche@gmail.com>
2020-07-14 19:46:08 -0400

    Debugging moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,26 @@
 '''
-Input: a List of integers
-Returns: a List of integers
+U
+Input: a List of integers (positive or negative)
+Returns: a List of integers (altered list)
+
+Move non-zero integers to the left **OR** move zeroes to the right
+Alter in place or create 'results' list
+
+P
+Iterate through int with loop
+If int == 0
+    pop int
+    append 0
+return array
 '''
+
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    for num in range(len(arr) - 1):
+        if arr[num] == 0:
+            arr.pop(num)
+            arr.append(0)
+    return arr
 
 
 if __name__ == '__main__':
