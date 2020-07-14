8d5c5040afd938ef9b5c838cb8fe1665e29239be
roselandroche <r.landroche@gmail.com>
2020-07-14 19:10:09 -0400

    Solution for single_number functional

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,20 @@
 '''
-Input: a List of integers where every int except one shows up twice
-Returns: an integer
+U
+Input: a List of integers (positive or negative)
+    - every int except one shows up twice
+Returns: the only integer without a double
+
+P
+For loop to iterate through integers
+Use method of arr.count(int)
+If the current integer count == 2, continue
+If the current integer count = 1, return current integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
+    for num in arr:
+        if arr.count(num) == 1:
+            return num
 
 
 if __name__ == '__main__':
