3315cdb9fc2503ad74aae065a640c499d317240c
roselandroche <r.landroche@gmail.com>
2020-07-14 21:41:19 -0400

    Functional product_of_all_other_numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -15,21 +15,22 @@ Return new arr
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
-    result = []
+    result = [0] * len(arr)
     product = 1
-    current_int = arr[0]
-    for num in range(len(arr) -1):
-        if num != current_int:
-            product *= arr[num]
-            result.append(product)
-            current_int += 1
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if j != i:
+                product *= arr[j]
+        result[i] = product
+        product = 1
         
     return result
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 2, 3, 4, 5]
+    arr = [4, 5]
+    # arr = [1, 2, 3, 4, 5]
     # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
