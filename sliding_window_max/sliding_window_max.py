801ae4471e144b72b699bcf6f635a85ed1b30b0b
roselandroche <r.landroche@gmail.com>
2020-07-16 19:26:22 -0400

    Functional solution for sliding_window_max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,12 +1,36 @@
 '''
-Input: a List of integers as well as an integer `k` representing the size of the sliding window
+U
+Input: a List of integers as well as an integer `k` representing 
+    the size of the sliding window
 Returns: a List of integers
+
+The window is k blocks wide.
+The window covers list int of amount k.
+Window moves from left to right one block/list item at a time.
+Returned value of list, of max values of items covered by window as it moves.
+
+P
+Create mini list (aka window)
+Move window through array
+Call max at first position
+As moves to the right, re-get max
+If it is, new max added to results
+If list ends, return results
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    result = []
+    start = 0
+    end = k
 
-    pass
+    while end <= len(nums):
+        window = nums[start:end]
+        max_num = max(window)
+        result.append(max_num)
+        start+=1
+        end+=1
 
+    return result
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
